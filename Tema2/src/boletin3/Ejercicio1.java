93d10fa1f82def5cc784f5caae2456c5183f8a55
roman.saborido <roman.saborido@106-28.info2>
2024-10-09 10:20:02 +0200

    Cambios

--- Tema2/src/boletin3/Ejercicio1.java
+++ Tema2/src/boletin3/Ejercicio1.java
@@ -1,10 +1,31 @@
 package boletin3;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
 		
-		//
+		// Creamos la variable numero para almacenar el número que introducirá el usuario
+		int numero;
+		
+		// Creamos el scanner
+		Scanner reader = new Scanner(System.in);
+		
+		// Pedimos al usuario que introduzca el número
+		System.out.println("Introduzca un número entero comprendido entre 0 y 9999");
+		numero = reader.nextInt();
+		
+		// if (numero < 0 || numero > 9999) 
+		if (numero < 0 || numero > 9999) {
+			System.out.println("Introduzca un número válido");
+		} else {
+			if (numero >= 0 && numero <= 9) {
+				System.out.println("El número es capicúa");
+			} if else {
+		}
+		
+		
 
 	}
 
